cc36da757a8a7eaeb6cbd93b6bfc03025313554f
dexkode4 <dexkode4@gmail.com>
2020-11-04 20:51:17 +0100

    updated directory images

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
     sections: [
         {
             title: "hats",
-            imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+            imageUrl: "/images/aman-jakhar.jpg",
             id: 1,
             linkUrl: "shop/hats",
         },
         {
             title: "jackets",
-            imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+            imageUrl: "/images/hana-brannigan.jpg",
             id: 2,
             linkUrl: "shop/jackets",
         },
         {
             title: "sneakers",
-            imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+            imageUrl: "/images/aj-nakasone.jpg",
             id: 3,
             linkUrl: "shop/sneakers",
         },
         {
             title: "womens",
-            imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+            imageUrl: "/images/ali-pazani.jpg",
             size: "large",
             id: 4,
             linkUrl: "shop/womens",
         },
         {
             title: "mens",
-            imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+            imageUrl: "/images/wallace-chuck.jpg",
             size: "large",
             id: 5,
             linkUrl: "shop/mens",
